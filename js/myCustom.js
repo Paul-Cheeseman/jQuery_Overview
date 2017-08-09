cd9e4e170b8be4276555e1572eda5fabd5168e5a
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2017-08-09 17:41:58 +0100

    initial responsiveness working for small devices

--- js/myCustom.js
+++ js/myCustom.js
@@ -29,3 +29,41 @@ $(".domHighlight").click(function(e) {
     //stop events being passed up the chain
     e.stopPropagation();
 });
+
+
+
+
+/*********************************************************************************
+  This section is to change the section sizes based on media queries to enhance the
+  responsiveness of the site to the end user devices.
+
+  Also the modals are too loarge for small mobile devices so will be deactived on 
+  them with an accompanying message
+**********************************************************************************/
+
+// Very Small Devices (Phones)
+if ($(window).width() < 769){
+    $(".interactive-elements").remove();
+    $("figcaption").remove();    
+    $("img").replaceWith("<em>IMG's removed from mobile view because they only apply to the interactive elements which cannot be viewed on a small mobile device</em>");
+
+     window.alert("Due to the small device size the interactive elements have been removed to prevent poor user experience.");
+     //remove modal links from menu
+     //put up message
+
+
+
+}
+// Small Devices (Tablet)
+else if ($(window).width() < 991){
+     window.alert("small device - tablet");
+     //put up message about "as you maybe viewing this on a tablet, and as such not have access to dev tools, you may not be able to perform some aspects of the practical exerices"
+}
+// Medium Devices (Laptop)
+else if ($(window).width() < 1199){
+     window.alert("medium device - tablet");
+}
+//Large Devices (Desktops)
+else{
+    window.alert("Large device - tablet");   
+}
